235c681be16140ff405d6b5e468ddf64ab4865fc
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-16 20:36:27 +0530

    feat: dom selection

--- Bluck.js
+++ Bluck.js
@@ -1,1 +1,6 @@
+var dob = document.querySelector("#b-date");
+var luckyNo = document.querySelector("#lucky-no");
+var calcBtn = document.querySelector(".calc-btn");
+var outputDiv = document.querySelector(".output-div");
 
+console.log(dob, luckyNo);
